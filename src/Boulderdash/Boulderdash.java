a18ed4c615bb1fc76879ae789516e43976a0f016
Nils <nilsgrimmer@gmx.de>
2014-05-01 16:05:31 +0200

    First Uniplay engine start

--- src/Boulderdash/Boulderdash.java
+++ src/Boulderdash/Boulderdash.java
@@ -1,11 +1,26 @@
 package Boulderdash;
 
+import Uniplay.Base.NG2DGameEngine;
 import Uniwork.Base.NGObject;
 
 public class Boulderdash extends NGObject {
 
+    protected NG2DGameEngine FGameEngine;
+
     public Boulderdash() {
         super();
+        FGameEngine = new NG2DGameEngine(this);
+
+    }
+
+    public void Run() {
+        FGameEngine.Initialize();
+        FGameEngine.Run();
+    }
+
+    public void Stop() {
+        FGameEngine.Stop();
+        FGameEngine.Finalize();
     }
 
 }
--- src/Boulderdash/Main.java
+++ src/Boulderdash/Main.java
@@ -8,16 +8,25 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    protected Boulderdash FBoulderdash;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Boulderdash");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+        FBoulderdash = new Boulderdash();
+        FBoulderdash.Run();
     }
 
+    @Override
+    public void stop() throws Exception {
+        FBoulderdash.Stop();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }

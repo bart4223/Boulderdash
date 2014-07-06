434d7bb82487be080dd21a43464e6f0859b256d7
Nils <nilsgrimmer@gmx.de>
2014-07-06 12:44:17 +0200

    WorkbenchManager EventHandler Kernel Initialized

---
+++ src/Boulderdash/BoulderdashModule.java
@@ -0,0 +1,12 @@
+package Boulderdash;
+
+import Uniplay.Kernel.NGGameEngineModule;
+import Uniplay.Kernel.NGGameEngineModuleManager;
+
+public class BoulderdashModule extends NGGameEngineModule {
+
+    public BoulderdashModule(NGGameEngineModuleManager aManager, String aName) {
+        super(aManager, aName);
+    }
+
+}

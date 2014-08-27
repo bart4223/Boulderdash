5b084068c5301362ee1bc39e176b1e0860ec4639
Nils <nilsgrimmer@gmx.de>
2014-08-27 20:40:22 +0200

    Sprite Bender

--- src/Boulderdash/Storage/BoulderdashSpriteBender.java
+++ src/Boulderdash/Storage/BoulderdashSpriteBender.java
@@ -2,11 +2,41 @@ package Boulderdash.Storage;
 
 public class BoulderdashSpriteBender extends BoulderdashCustomSprite {
 
-    public final static Integer ID  = 1;
+    public enum Mode {footdown, footup};
+
+    public final static Integer ID    = 1;
+    public final static Integer ID_UP = 3;
+
+    protected Mode FMode;
 
     public BoulderdashSpriteBender() {
         super();
-        FID = ID;
+        setMode(Mode.footdown);
+    }
+
+    public Mode getMode() {
+        return FMode;
+    }
+
+    public void ToggleMode() {
+        if (FMode == Mode.footdown) {
+            setMode(Mode.footup);
+        }
+        else {
+            setMode(Mode.footdown);
+        }
+    }
+
+    public void setMode(Mode aMode) {
+        FMode = aMode;
+        switch (FMode) {
+            case footdown:
+                FID = ID;
+                break;
+            case footup:
+                FID = ID_UP;
+                break;
+        }
     }
 
 }
